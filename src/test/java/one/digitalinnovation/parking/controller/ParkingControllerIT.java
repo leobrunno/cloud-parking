1a2a8fd62babe6c6ef868fd4f17e957e85b5c4d6
leobrunno <leobrunno@live.com>
2022-12-08 20:33:20 -0300

    Test: Implementação de teste para endpoint create

--- src/test/java/one/digitalinnovation/parking/controller/ParkingControllerIT.java
+++ src/test/java/one/digitalinnovation/parking/controller/ParkingControllerIT.java
@@ -1,11 +1,14 @@
 package one.digitalinnovation.parking.controller;
 
 import io.restassured.RestAssured;
+import one.digitalinnovation.parking.model.dto.ParkingCreateDTO;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ParkingControllerIT extends AbstractContainerBase {
@@ -27,5 +30,25 @@ class ParkingControllerIT extends AbstractContainerBase {
                 .then()
                 .statusCode(HttpStatus.OK.value())
                 .extract().response().body().prettyPrint();
+    }
+
+    @Test
+    void whenCreateThenCheckIsCreated() {
+        ParkingCreateDTO createDTO = new ParkingCreateDTO();
+        createDTO.setColor("Azul");
+        createDTO.setLicence("AWS-2890");
+        createDTO.setModel("Corcel");
+        createDTO.setState("RS");
+
+        RestAssured.given()
+                .header("authorization", "Basic dXNlcjpjcmZANTc0")
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createDTO)
+                .post("/parking")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("licence", Matchers.equalTo("AWS-2890"))
+                .body("model", Matchers.equalTo("Corcel"));
     }
 }
